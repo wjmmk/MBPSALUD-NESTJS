42e4935ec8a612a7ca7e3ac0ddcab98923c2f2ef
William Mosquera <wjmmk3@gmail.com>
2024-04-23 14:41:23 -0500

    initial-setup primas added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+
+  /**
+   * Implementation of Swagger
+  */
+  const config = new DocumentBuilder()
+  .setTitle('Project example')
+  .setDescription('API documentation and description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
